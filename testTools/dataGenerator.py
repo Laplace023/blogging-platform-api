2d329977691d3f810479bc649772a081fe3397c5
Laplace023 <genesiscabaltica.work@gmail.com>
2025-02-28 01:47:15 +0800

    cleaning

--- testTools/dataGenerator.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('blogs.db')
-cursor = conn.cursor()
-
-cursor.execute("INSERT INTO posts VALUES ('2', 'test', 'test', 'test', 'test', 'test', 'test')")
-
-conn.commit()
-conn.close()
--- testTools/testCreateNum.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('blogs.db')
-cursor = conn.cursor()
-
-cursor.execute("SELECT MAX(id) FROM posts")
-data = cursor.fetchone()
-print(data[0])
-print(type(data[0]))
